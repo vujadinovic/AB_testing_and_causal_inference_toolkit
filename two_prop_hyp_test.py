43fbf24d084d3ed49740308f77fd2fdc0028982b
Stefan Vujadinovic <59982660+vujadinovic@users.noreply.github.com>
2026-09-06 13:41:27 +0200

    Add files via upload

--- two_prop_hyp_test.py
+++ two_prop_hyp_test.py
@@ -5,3 +5,7 @@ def pooled_proportion(successes_a, total_a, successes_b, total_b):
     
 def pooled_standard_error(pooled_p, total_a, total_b):
     return math.sqrt(pooled_p*(1-pooled_p)* (1/total_a + 1/total_b))
+
+
+def two_proportion_z_statistic(p_a, p_b, pooled_se):
+    return (p_b - p_a) / (pooled_se)
